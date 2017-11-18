7fb24ed70acaa89753c99bb6ed91d0fad1f82461
cyberchevy <chevy.odell@gmail.com>
2017-11-17 23:11:42 -0700

    Add files via upload

--- list.cpp
+++ list.cpp
@@ -1,4 +1,4 @@
-// list.h
+// list.cpp
 // ENCM 339 Lab 8 Exercises C
 // Last edit: 11/17/2017
 // Produced by: Shishir Srivastava and Siobhan O'Dell
@@ -9,99 +9,198 @@
 using namespace std;
 
 //Default Constructor
-FlowList::FlowList()
-	{
-		head = NULL;
-		ptr = head;
-	}
+	FlowList::FlowList()
+		{
+			head = NULL;
+			cursor = head;
+		}
 
 //Copy Constructor
-FlowList::FlowList(const FlowList& source)
-	{
-		copy(source);
-	}
+	FlowList::FlowList(const FlowList& source)
+		{
+			head = NULL;
+			copy(source);
+			reset_cursor();
+		}
 
 //Assignment Operator
-FlowList& FlowList::operator = (const FlowList& rhs)
-	{
-		if(this != &rhs)
+	FlowList& FlowList::operator = (const FlowList& rhs)
 		{
-			//delete the storage
-			//destroy();
 			
-			copy(rhs);
+			if(this != &rhs)
+			{
+				
+				//delete the storage
+				destroy();
+				
+				//copy in rhs
+				copy(rhs);
+			}
+			
+			reset_cursor();
+			return *this;
 		}
-		
-		return *this;
-	}
  
 //Destructor
-  //~FlowList(){}
-
-//Implementer Functions
-void FlowList::insert(const int& yearA, const double& flowA)
-	{
-
-		Node* temp = new Node;
-		assert(temp != 0);
-		temp -> item.year = yearA;
-		temp -> item.flow = flowA;
-		temp -> next = head;
-		head = temp;
-	  
-	}
-
-//void FlowList::remove(const int& yearA, const double& flowA){}
-  //  PROMISES:
-  //    If no node has an item matching itemA,list is unchanged.
-  //    Otherwise exactly one node with its item == itemA is removed.
-
-void FlowList::display()
-{
-	ptr = head;
-	
-	while(ptr)
-	{
-		
-		cout << "YEAR: ";
-		cout << ptr -> item.year;
-		
-		cout << "FLOW: ";
-		cout << ptr -> item.flow;
-		
-		cout << endl;
-		
-		ptr = ptr -> next;
-	}
-	
-	cout << endl;
-}
-
-
-//void FlowList::destroy(){}
-  // Deallocate all nodes, set headM to zero.
-
-void FlowList::copy(const FlowList& source)
-{
-	Node* move = source.head;
-	
-	while(move)
+	FlowList::~FlowList()
 		{
+			
+			//destroy();
+			//cout << "SUCCESS" << endl;
+		}
+
+//Getters and Setters
+	int FlowList::get_year() const 
+		{
+			if(cursor)
+				return cursor -> item.year;
+			return 0;
+		}
+		
+	double FlowList::get_flow() const 
+		{	
+			if(cursor)
+				return cursor -> item.flow;
+			return 0;
+		}
+		
+//Implementer Functions
+	void FlowList::insert(const int& yearA, const double& flowA)
+		{
+
 			Node* temp = new Node;
+			assert(temp != 0);
+			temp -> item.year = yearA;
+			temp -> item.flow = flowA;
+			temp -> next = head;
+			head = temp;
+			reset_cursor();
+		}
+
+	int FlowList::remove(const int& yearA)
+		{
+			Node* ptrprev;
 			
-			temp -> item.year = move -> item.year;
-			temp -> item.flow = move -> item.flow;
+			Node* ptr = head;
+				
+			if(ptr -> item.year == yearA)
+				{
+					head = head -> next;
+					delete ptr;
+					return 1;
+				}
 			
-			temp -> next = this -> head;
+			ptrprev = ptr;
+			ptr = ptr -> next;
 			
-			this -> head = temp;
+			while(ptr)
+				{
+
+					if(ptr -> item.year == yearA)
+						{
+							ptrprev -> next = ptr -> next;
+							delete ptr;
+							return 1;
+						}
+					
+					ptrprev = ptr;
+					ptr = ptr -> next;
 			
-			move = move -> next;
+				}
+				
+			reset_cursor();
+			return 0;
+		}
+
+	void FlowList::display()
+		{
+			Node* ptr =	head;
 			
-			if(!move)
-				temp -> next = NULL;
+			while(ptr)
+				{
+					
+					cout << "YEAR: ";
+					cout << ptr -> item.year;
+					
+					cout << "    FLOW: ";
+					cout << ptr -> item.flow;
+					
+					cout << endl;
+					
+					cout << "Cursor is pointing to YEAR: ";
+					cout << cursor -> item.year;
+					
+					cout << endl;
+					
+					ptr = ptr -> next;
+				}
+			
+			cout << endl;
+		}
+	
+	int FlowList::move_cursor()
+		{
+			if(cursor -> next)
+				{
+					cursor = cursor -> next;
+					return 1;
+				}
+				
+			else
+				return 0;
+		}
+	
+	void FlowList::reset_cursor()
+		{
+			 cursor = head;
 			
 		}
 		
-	this -> head = source.head;
-}
+	void FlowList::destroy()
+		{
+
+			Node* ptr = head -> next;
+			//cout << "1ptr is at: " << ptr -> item.year << endl;
+			
+			while(ptr)
+				{	
+					//cout << "2ptr is at: " << ptr -> item.year << endl;
+					cout << "^DELETE YEAR: " << head -> item.year << endl;
+					delete head;
+					//cout << "3ptr is at: " << ptr -> item.year << endl;
+					head = ptr;
+					//cout << "4ptr is at: " << ptr -> item.year << endl;
+					ptr = head -> next;
+					//cout << "5ptr is at: " << ptr -> item.year << endl;
+
+				}
+			
+			cout << "DELETE YEAR: " << head -> item.year << endl;
+			delete head;
+			head = NULL;
+		}
+
+	void FlowList::copy(const FlowList& source)
+		{
+			Node* move = source.head;
+			
+			while(move)
+				{
+					Node* temp = new Node;
+					
+					temp -> item.year = move -> item.year;
+					temp -> item.flow = move -> item.flow;
+					
+					temp -> next = this -> head;
+					
+					this -> head = temp;
+					
+					move = move -> next;
+					
+					if(!move)
+						temp -> next = NULL;
+					
+				}
+				
+			this -> head = source.head;
+		}
