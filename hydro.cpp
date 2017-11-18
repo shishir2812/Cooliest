2530e659b5c66d908eeb15659bb838aea2ed0c6c
shishir2812 <shishir2812@gmail.com>
2017-11-17 21:42:32 -0700

    Add files via upload

--- hydro.cpp
+++ hydro.cpp
@@ -11,24 +11,50 @@ Lab Section: B03
 
 using namespace std;
 
-#include "hydro.h"
-#include "list.cpp"
 #include "list.h"
+
+#include "hydro.h"
  
-int main(void)
+int main()
 {
+	FlowList flow;
 	displayHeader();
-	int number_of_records = readData();
+	int numRecords = readData(flow);
+	int quit = 0;
+	while (1)
+	{
+		switch(menu())
+		{
+			case 1:
+			//calls display function
+			display(flow);
+			pressEnter();
+			break;
+			
+			case 5:
+			cout << "program terminated" << endl;
+			quit = 1;
+			break;
+			
+			default:
+			cout << "not a valid input" << endl;
+			break;
+			
+			
+		}
+		if (quit==1)
+			break;
+	}
 	
 }
 
 void displayHeader()
 {
-	cout<<"Program: Flow Studies- Fall 2017\n Version:1.0 \n Lab section: B03 \n Producted by: Shishir Srivastava and Shiobhan Odell\n <<Press Enter to Continue>>"<<endl;
-	
+	cout<<"Program: Flow Studies- Fall 2017\n Version:1.0 \n Lab section: B03 \n Producted by: Shishir Srivastava and Shiobhan Odell\n "<<endl;
+	pressEnter();
 }
 
-int readData()
+int readData(FlowList &flow)
 {
 	int num= 0;
 	int y;
@@ -44,17 +70,45 @@ int readData()
 	}
 	//inserts the date and year into the ListItem structure
 	
-	/*
+	
 	while (inFlow >> y >> d)
 	{
-		Flowlist.insert(y,d);
+		flow.insert(y,d);
 		return num;
 	}
 	
 	
-	*/
+	
 	inFlow.close();
 		
 	return num;
 }
+
+
+int menu()
+{
+	int choice;	
+	cout <<"1. Display flow list, average, and median"<< endl;
+	cout <<"2. Add data"<< endl;
+	cout <<"3. Save data into the file"<< endl;
+	cout <<"4. Remove data << endl" <<endl;	
+	cout <<"5. Quit" <<endl;	
+	cout << "Enter your choice (1, 2, 3, 4, 5)"  <<endl;	
+	cin >> choice;
+	return choice;
+}
+
+void pressEnter()
+{
+	cout <<"\n<<< Press enter to continue>>>" << endl;
+	cin.get();
+}
+
+
+void display(const FlowList &flow)
+{
+	
+	
+}
+
 
--- hydro.h
+++ hydro.h
@@ -5,9 +5,20 @@
 void displayHeader();
 //promises: display the introduction screen
 
-int readData();
+int readData(FlowList &flow);
 /*promises: to read record year, inserts them into the list, and returns the
 number of records in the file.
+*/
+
+int menu();
+//promises: to display the menu screen and return the user's choice.
+
+void pressEnter();
+//promises: waits for the user to press enter
+
+void display(const FlowList &flow);
+/*promises: displays the list in ascending order based on the flow data.
+			Calls average and median function, and displays their data.
 */
 
 
