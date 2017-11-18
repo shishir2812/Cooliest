1b732f3b119ea897a82bfd716be0b1808543e3b2
shishir2812 <shishir2812@gmail.com>
2017-11-17 20:00:22 -0700

    Add files via upload

--- hydro.cpp
+++ hydro.cpp
@@ -8,25 +8,53 @@ Lab Section: B03
 #include <iostream>
 #include <fstream>
 #include <string>
+
+using namespace std;
+
 #include "hydro.h"
 #include "list.cpp"
 #include "list.h"
-using std::cout;
-using std::cin;
-using std::endl;
-
  
 int main(void)
 {
 	displayHeader();
-	
+	int number_of_records = readData();
 	
 }
 
 void displayHeader()
 {
-	cout<<" Program: Flow Studies- Fall 2017\n Version:1.0 \n Lab section: B03 \n Producted by: Shishir Srivastava and Shiobhan Odell\n <<Press Enter to Continue>>"<<std::endl;
+	cout<<"Program: Flow Studies- Fall 2017\n Version:1.0 \n Lab section: B03 \n Producted by: Shishir Srivastava and Shiobhan Odell\n <<Press Enter to Continue>>"<<endl;
+	
+}
+
+int readData()
+{
+	int num= 0;
+	int y;
+	double d;	
+	ifstream inFlow;
+	inFlow.open("flow.txt",ios::in);
+	
+	//unsuccessful at opening
+	if (inFlow.fail())
+	{
+		cerr<<"Unsuccesful in opening flow.txt"<<endl;
+		exit(1);
+	}
+	//inserts the date and year into the ListItem structure
+	
+	/*
+	while (inFlow >> y >> d)
+	{
+		Flowlist.insert(y,d);
+		return num;
+	}
 	
 	
+	*/
+	inFlow.close();
+		
+	return num;
 }
 
--- hydro.h
+++ hydro.h
@@ -1,12 +1,14 @@
 #ifndef hydro_h
 #define hydro_h
 
+
 void displayHeader();
 //promises: display the introduction screen
 
-
-
-
+int readData();
+/*promises: to read record year, inserts them into the list, and returns the
+number of records in the file.
+*/
 
 
 #endif
