d86398629fa47c2c719c37d94c391f0e047f5d5b
shishir2812 <shishir2812@gmail.com>
2017-11-17 18:29:17 -0700

    Add files via upload

---
+++ hydro.cpp
@@ -0,0 +1,32 @@
+/*
+Name: Shishir Srivastava and Siobhan Odell
+Version 1.0
+Lab Section: B03
+
+ */ 
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "hydro.h"
+#include "list.cpp"
+#include "list.h"
+using std::cout;
+using std::cin;
+using std::endl;
+
+ 
+int main(void)
+{
+	displayHeader();
+	
+	
+}
+
+void displayHeader()
+{
+	cout<<" Program: Flow Studies- Fall 2017\n Version:1.0 \n Lab section: B03 \n Producted by: Shishir Srivastava and Shiobhan Odell\n <<Press Enter to Continue>>"<<std::endl;
+	
+	
+}
+
---
+++ hydro.h
@@ -0,0 +1,12 @@
+#ifndef hydro_h
+#define hydro_h
+
+void displayHeader();
+//promises: display the introduction screen
+
+
+
+
+
+
+#endif
